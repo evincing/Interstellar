5fe2515de99a83def924026adde4805ef7f47253
Jade <quartzyeti@disroot.org>
2026-02-27 11:48:15 -0600

    Add admin and guest users to config

--- config.js
+++ config.js
@@ -2,7 +2,8 @@ const config = {
   challenge: false, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: "password",
+    admin: "playstation$447",
+    guest: "super"
   },
 };
 
